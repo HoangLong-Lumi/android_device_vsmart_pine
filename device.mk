15731659d66ddc06d01d2830230957f2b73102b9
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-09-02 09:54:56 +0700

    casuarina: Update from V430A_OPN_U_B13_210315

--- device.mk
+++ device.mk
@@ -19,5 +19,13 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.casuarina.rc
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/pronto_wlan.ko:$(TARGET_COPY_OUT_SYSTEM)/lib/modules/pronto_wlan.ko
+
 # Inherit from sdm632-common
 $(call inherit-product, device/vsmart/sdm632-common/sdm632.mk)
